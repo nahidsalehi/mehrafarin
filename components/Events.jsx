24c3444af1fb8990ec8d91dab18c4ada2818b7d8
nahidsalehi <salehinahid7@gmail.com>
2022-08-15 16:59:12 +0430

    please

--- components/Events.jsx
+++ components/Events.jsx
@@ -1,7 +1,7 @@
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import EventsData from '../pages/eventsData';
+import EventsData from '../public/eventsData';
  
 
  function Events(){
